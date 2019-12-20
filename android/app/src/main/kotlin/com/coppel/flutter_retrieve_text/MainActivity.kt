2c464dd21863a4a0bb45e756b91a66d717d3fb35
gcastro <gcastro@coppel.com>
2019-12-19 17:58:42 -0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/coppel/flutter_retrieve_text/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.coppel.flutter_retrieve_text
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
